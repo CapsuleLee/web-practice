508df0c09d307b7f246ef077a8cfe00a820c0716
SangMin LEE <163146180+CapsuleLee@users.noreply.github.com>
2024-09-27 14:42:20 +0900

    week4
    실습

---
+++ blackjack.js
@@ -0,0 +1,45 @@
+
+
+let cardOne = 7;
+let cardTwo = 5;
+let sum = cardOne + cardTwo;
+let cardOneBank = 7;
+let cardTwoBank = 5;
+let cardThreeBank = 6;
+let cardFourBank = 1;
+
+let cardThree = 9;
+sum += cardThree;
+if (sum > 21){
+    console.log(`You have ${sum} points`);
+    console.log("You lost");
+    return
+} else if (sum ===21){
+    console.log(`You have ${sum} points`);
+    console.log("You win");
+    return;
+}
+console.log(`You have ${sum} points`);
+
+let bankSum = cardOneBank + cardTwoBank + cardThreeBank + cardFourBank;
+let extraCard = 10;
+while (bankSum<17){
+    bankSum += extraCard;
+}
+
+console.log(`Bank have ${bankSum} points`);
+if (bankSum>21){
+    console.log("You win");
+    return
+}
+if (bankSum === sum){
+    console.log("Draw");
+    return
+}
+
+
+if (bankSum > 21 || (sum <= 21 && sum > bankSum)){
+    console.log("You win");
+} else {
+    console.log("Bank wins");
+}
