dbc3ffce94c06f4aa497f4be7d90ce47fc4b4395
SangMin LEE <163146180+CapsuleLee@users.noreply.github.com>
2024-11-08 16:53:32 +0900

    week10
    이게 기본값임

--- browser-extension/src/index.js
+++ browser-extension/src/index.js
@@ -1,3 +1,6 @@
+import axios from 'axios';
+
+
 // form fields
 const form = document.querySelector('.form-data');
 const region = document.querySelector('.region-name');
@@ -11,6 +14,57 @@ const fossilfuel = document.querySelector('.fossil-fuel');
 const myregion = document.querySelector('.my-region');
 const clearBtn = document.querySelector('.clear-btn');
 
+const calculateColor = async (value) => {
+    let co2Scale = [0, 150, 600, 750, 800];
+    let colors = ['#2AA364', '#F5EB4D', '#9E4229', '#381D02', '#381D02'];
+    let closestNum = co2Scale.sort((a, b) => {
+    return Math.abs(a - value) - Math.abs(b - value);
+    })[0];
+    console.log(value + ' is closest to ' + closestNum);
+    let num = (element) => element > closestNum;
+    let scaleIndex = co2Scale.findIndex(num);
+    let closestColor = colors[scaleIndex];
+    console.log(scaleIndex, closestColor);
+    chrome.runtime.sendMessage({ action: 'updateIcon', value: { color: closestColor } });
+};
+   
+
+
+
+async function displayCarbonUsage(apiKey, region){
+    try {
+        await axios
+        .get('https://api.co2signal.com/v1/latest', {
+        params: {
+        countryCode: region,
+        },
+        headers: {
+        //please get your own token from CO2Signal https://www.co2signal.com/
+        'auth-token': apiKey,
+        },
+        })
+        .then((response) => {
+        let CO2 = Math.floor(response.data.data.carbonIntensity);
+        calculateColor(CO2);
+        loading.style.display = 'none';
+        form.style.display = 'none';
+        myregion.textContent = region;
+        usage.textContent =
+        Math.round(response.data.data.carbonIntensity) + ' grams (grams C02 emitted per kilowatt hour)';
+        fossilfuel.textContent =
+        response.data.data.fossilFuelPercentage.toFixed(2) +
+        '% (percentage of fossil fuels used to generate electricity)';
+        results.style.display = 'block';
+        });
+        } catch (error) {
+        console.log(error);
+        loading.style.display = 'none';
+        results.style.display = 'none';
+        errors.textContent = 'Sorry, we have no data for the region you have requested.';
+        }
+}
+
+
 function setUpUser(apiKey, regionName) {
     localStorage.setItem('apiKey', apiKey);
     localStorage.setItem('regionName', regionName);
@@ -19,6 +73,7 @@ function setUpUser(apiKey, regionName) {
     clearBtn.style.display = 'block';
     displayCarbonUsage(apiKey, regionName);
  }
+
 
 function handleSubmit(e) {
     e.preventDefault();
@@ -29,6 +84,14 @@ function init() {
     const storedApiKey = localStorage.getItem('apiKey');
     const storedRegion = localStorage.getItem('regionName');
     //set icon to be generic green
+
+    chrome.runtime.sendMessage({
+        action: 'updateIcon',
+            value: {
+                color: 'green',
+            },
+     });
+    
     //todo
     if (storedApiKey === null || storedRegion === null) {
         form.style.display = 'block';
